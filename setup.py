f531bce54a2c5eb9f6762fa373e24e986dc4ab18
Alex Clark <aclark@aclark.net>
2010-10-22 09:15:36 -0400

    What kind of black magic is required to get this to work?

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 name = 'parse2plone'
 description = 'Easily import static HTML files into Plone.' 
-version = '0.1'
+version = '0.2'
 
 def read(file):
     file = open(file)
