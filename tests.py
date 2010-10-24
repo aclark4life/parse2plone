665e335e5dd9c32e5c03505cbdb18a2c609aec54
Alex Clark <aclark@aclark.net>
2010-10-24 15:56:18 -0400

    Make it clear we'd like to add a test to make sure command line argument parsing works as expect

--- tests.py
+++ tests.py
@@ -2,24 +2,10 @@ import unittest
 
 def test_suite():
     suite = unittest.TestSuite()
-    suite.addTest(TestCase('foo'))
-    suite.addTest(TestCase('bar'))
-    suite.addTest(TestCase('bar'))
+    suite.addTest(TestCase('parse_args'))
     return suite
 
 class TestCase(unittest.TestCase):
 
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-    def foo(self):
-        pass
-
-    def bar(self):
-        pass
-
-    def baz(self):
+    def parse_args(self):
         pass
