eb68dd4d7fbed50b12ea88e8ea15efbf4cd8ebe8
Alex Clark <aclark@aclark.net>
2010-10-25 16:31:32 -0400

    Onwards to 0.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 name = 'parse2plone'
 description = 'Easily import static HTML files into Plone.' 
-version = '0.7'
+version = '0.8'
 
 def read(file):
     file = open(file)
