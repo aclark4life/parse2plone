c2a7b6ee3152e080e1dcad24a12de4c78c68f7d8
Alex Clark <aclark@aclark.net>
2010-11-03 12:38:31 -0400

    Mock up first test

--- tests.py
+++ tests.py
@@ -1,12 +1,17 @@
 import unittest
 
-class IntegerArithmenticTestCase(unittest.TestCase):
-    def testAdd(self):  ## test method names begin 'test*'
-        self.assertEquals((1 + 2), 3)
-        self.assertEquals(0 + 1, 1)
-    def testMultiply(self):
-        self.assertEquals((0 * 10), 0)
-        self.assertEquals((5 * 8), 40)
+from zc.buildout import testing
+
+class ConfigOptionsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def testCustomPath(self):
+        # The first thing we want to test is that if someone
+        # customizes the path, their customization is saved in
+        # the _SETTINGS dict (and used in Parse2Plone).
+        pass
 
 if __name__ == '__main__':
     unittest.main()
