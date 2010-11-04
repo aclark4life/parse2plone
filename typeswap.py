60c729f2026decf3dd7d9431effa553fbe11098c
Alex Clark <aclark@aclark.net>
2010-11-03 20:01:54 -0400

    Mock up "typeswap" feature

---
+++ typeswap.py
@@ -0,0 +1,12 @@
+
+###############################################################################
+#                                                                             #
+# Adds "typeswap" feature to ``parse2plone``.                                 #
+#                                                                             #
+# This feature allows the user to customize content types used by             #
+# ``parse2plone`` when creating content in Plone, by specifying a "default"   #
+# and corresponding "custom" content type (e.g.                               #
+# --typeswap=Document:MyCustomPageType).                                      #
+#                                                                             #
+###############################################################################
+
