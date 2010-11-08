1d6402a75358f822e0abf922b66045c15fcf254c
Alex Clark <aclark@aclark.net>
2010-11-08 09:08:23 -0500

    PEP8 fix

--- tests.py
+++ tests.py
@@ -10,7 +10,7 @@ class ConfigOptionsTestCase(unittest.TestCase):
     def testCustomPath(self):
         # The first thing we want to test is that if someone
         # customizes the path, their customization is saved in
-        # the _SETTINGS dict. 
+        # the _SETTINGS dict.
         pass
 
 if __name__ == '__main__':
