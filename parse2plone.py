f66d37d303b4e0de1ea619faafddb63762520023
Alex Clark <aclark@aclark.net>
2010-11-09 17:05:57 -0500

    Clean up

--- parse2plone.py
+++ parse2plone.py
@@ -106,16 +106,16 @@ def match_files(files, base, match):
     return results
 
 
-#These next two functions add rename support to ``parse2plone``.
+# These next two functions add rename support to ``parse2plone``.
 #
-#This feature allows the user to specify two paths: old and new (e.g.
-#--rename=old:new ).
+# This feature allows the user to specify two paths: old and new (e.g.
+# --rename=old:new ).
 #
-#Then if a path like this is found:
+# Then if a path like this is found:
 #    /old/2000/01/01/foo/index.html
 #
-#Instead of creating /old/2000/01/01/foo/index.html (in Plone),
-#``parse2plone`` will create:
+# Instead of creating /old/2000/01/01/foo/index.html (in Plone),
+# ``parse2plone`` will create:
 #
 #    /new/2000/01/01/foo/index.html
 
